4f129a0fe222d9d48bcc291eb45cf6dfb73bfbb5
Nathan Sousa <nathan.wks@outlook.com>
2024-10-18 02:23:37 -0300

    updates vs ig

--- src/CashFlow.Exception/obj/Debug/net8.0/CashFlow.Exception.AssemblyInfo.cs
+++ src/CashFlow.Exception/obj/Debug/net8.0/CashFlow.Exception.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Exception")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46a30e40c61a16e276eeca3c436fc239c1be970e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Exception")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Exception")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
