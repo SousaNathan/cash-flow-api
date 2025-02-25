957dc9dca199dfd4705c7616285f14cfb5b44901
Nathan Sousa <nathan.wks@outlook.com>
2025-02-25 00:13:33 -0300

    create report controller

---
+++ src/CashFlow.API/Controllers/ReportController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace CashFlow.API.Controllers;
+
+[Route("cashflow-api/report")]
+[ApiController]
+public class ReportController : Controller
+{
+    [HttpGet("excel")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetExcel()
+    {
+        byte[] file = new byte[1];
+
+        if (file.Length > 0)
+        {
+            return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+        }
+
+        return NoContent();
+    }
+}
